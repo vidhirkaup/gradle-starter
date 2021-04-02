3fb194fb1a0a917464dc96a52928d960a156b373
Vidhir Kaup <vidhir.kaup@gmail.com>
2021-04-02 11:58:22 -0400

    initial commit

---
+++ src/main/java/Greeter.java
@@ -0,0 +1,5 @@
+public class Greeter {
+    public String greet(String message){
+        return message;
+    }
+}
---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.greet("hello world @ " + new LocalTime()));
+    }
+}
